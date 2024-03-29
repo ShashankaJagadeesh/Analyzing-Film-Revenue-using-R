494b817ed4fbaefcf0c393ce8eb8ee9e4bc3f352
ShashankaJagadeesh <165324583+ShashankaJagadeesh@users.noreply.github.com>
2024-03-29 13:30:08 +0000

    Add files via upload

---
+++ Code.R
@@ -0,0 +1,14 @@
+
+disney_data <- read.csv(file="C:/Users/PC/Documents/Assignments/R programming/Assignment 2/Datasets/disney_plus_titles.csv")
+
+imdb_data <- read.csv(file="C:/Users/PC/Documents/Assignments/R programming/Assignment 2/Datasets/imdb_movies.csv")
+
+netflix_data <- read.csv(file="C:/Users/PC/Documents/Assignments/R programming/Assignment 2/Datasets/netflix_titles.csv")
+
+disney_data.head()
+
+library(dplyr)
+library(tidyr)
+library(tm)
+library(tidytext)
+
